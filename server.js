3a6b8bef94447c5cfdf054411840db4d1ac5cbdf
ahmedawd007 <85526746+ahmedawd007@users.noreply.github.com>
2025-10-03 00:53:36 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+{
+  "name": "whatsapp-webhook",
+  "version": "1.0.0",
+  "description": "WhatsApp Bot Webhook",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
